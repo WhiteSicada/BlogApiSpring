0110fc50a66168d262e8875b833085140e7b4b84
WhiteSicada <dino_villa@hotmail.fr>
2021-08-12 03:13:50 +0200

    init

---
+++ src/main/java/com/example/blogapi/controller/AuthController.java
@@ -0,0 +1,30 @@
+
+package com.example.blogapi.controller;
+import com.example.blogapi.service.AuthService;
+import com.example.blogapi.dto.LoginRequest;
+import com.example.blogapi.dto.RegisterRequest;
+import com.example.blogapi.service.AuthenticationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+}
